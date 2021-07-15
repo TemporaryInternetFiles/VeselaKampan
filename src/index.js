81151a59a66fe204acb38efcc82d6de13cb27453
Ondřej Kotas <ok@krtkovo.eu>
2021-07-15 02:02:34 +0200

    grab the sauce code

--- src/index.js
+++ src/index.js
@@ -1,81 +1,152 @@
 import { claims, generators } from "./data.js";
+import { splitText, pickRandom } from "./helperFunctions.js";
 
-const splitText = (text, maxLineLength) => {
-  const result = [];
-  while (text.length > maxLineLength) {
-    let pos = text.substring(0, maxLineLength).lastIndexOf(" ");
-    pos = pos <= 0 ? maxLineLength : pos;
-    result.push(text.substring(0, pos));
-    let i = text.indexOf(" ", pos) + 1;
-    if (i < pos || i > pos + maxLineLength) i = pos;
-    text = text.substring(i);
-  }
-  result.push(text);
-  return result;
+const unrolledGenerators = generators.flatMap(({ url, weight }) => Array(weight).fill(url));
+
+const imageReader = new FileReader();
+const logo = new Image();
+logo.src = "https://piratskahomoskinheadskaplatforma.cz/VeselaKampan/public/logo.png";
+let currentImage = new Image();
+let currentText = "Test text";
+
+const rerollImage = async () => {
+  const imageData = await fetch(pickRandom(unrolledGenerators));
+
+  return new Promise((resolve) => {
+    const image = new Image();
+
+    image.addEventListener("load", () => {
+      currentImage = image;
+      resolve();
+    });
+
+    image.crossOrigin = "anonymous";
+    image.src = imageData.url;
+  });
 };
 
-const pickRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const rerollText = () => {
+  currentText = pickRandom(claims);
+};
 
 const canvas = document.getElementById("picture");
 const ctx = canvas.getContext("2d");
 const font = new FontFace("Bebas Neue", "url(https://piratskahomoskinheadskaplatforma.cz/VeselaKampan/public/BebasNeue-Bold.ttf)");
-const fontSize = 60;
 
 const initFont = async () => {
   await font.load();
   document.fonts.add(font);
-  ctx.font = `${fontSize}px 'Bebas Neue'`;
 };
 
-const unrolledGenerators = generators.flatMap(({ url, weight }) => Array(weight).fill(url));
+const setFile = (file) => {
+  if (!file.type.startsWith("image/")) {
+    return;
+  }
 
-const initImage = async (customText) => {
-  const loadLogo = () => {
-    const logo = new Image();
-    logo.src = "https://piratskahomoskinheadskaplatforma.cz/VeselaKampan/public/logo.png";
-    logo.addEventListener("load", () => {
-      ctx.drawImage(logo, 525, 20);
-      const linkSave = document.getElementById("save");
-      linkSave.setAttribute("download", "PirStanKampan.png");
-      setTimeout(() => {
-        linkSave.setAttribute("href", canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-      }, 500);
-    });
-  };
+  imageReader.readAsDataURL(file);
+};
 
-  const imageData = await fetch(pickRandom(unrolledGenerators));
-  const image = new Image();
-  image.crossOrigin = "anonymous";
-  image.src = imageData.url;
-  image.addEventListener("load", () => {
-    ctx.drawImage(image, 0, 0);
-    loadLogo();
-    const unsplitText = customText ?? pickRandom(claims);
-    const text = splitText(unsplitText, 20).reverse();
-    text.forEach((line, index) => {
-      const x = 30;
-      const y = 685;
-      const padding = 15;
-      const lineHeight = padding + fontSize;
-      ctx.fillStyle = "#f9dc4d";
-      ctx.fillRect(x, y - (index * lineHeight), ctx.measureText(line).width + 2 * padding, lineHeight);
-      ctx.textBaseline = "top";
-      ctx.fillStyle = "black";
-      ctx.fillText(line, x + padding, y + padding - (index * lineHeight));
-    });
+canvas.addEventListener("dragover", (e) => e.preventDefault());
+
+canvas.addEventListener("drop", (e) => {
+  e.preventDefault();
+  if (!e.dataTransfer || e.dataTransfer.files.length <= 0) {
+    return;
+  }
+
+  setFile(e.dataTransfer.files[0]);
+});
+
+const repaintImage = async () => {
+  // clear to black (for transparent images)
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // scale image to always fill the canvas
+  const scaleX = canvas.width / currentImage.width;
+  const scaleY = canvas.height / currentImage.height;
+  const scale = Math.max(scaleX, scaleY);
+  ctx.setTransform(scale, 0, 0, scale, 0, 0);
+  ctx.drawImage(currentImage, 0, 0);
+
+  ctx.setTransform(); // reset so that everything else is normal size
+  ctx.drawImage(logo, 525, 20);
+
+  const lines = splitText(currentText, 20).reverse();
+  const fontSize = lines.length < 5 ? 60 : 40;
+  ctx.font = `${fontSize}px 'Bebas Neue'`;
+  lines.forEach((line, index) => {
+    const x = 30;
+    const y = 685;
+    const padding = 15;
+    const lineHeight = padding + fontSize;
+    ctx.fillStyle = "#f9dc4d";
+    ctx.fillRect(x, y - (index * lineHeight), ctx.measureText(line).width + 2 * padding, lineHeight);
+    ctx.textBaseline = "top";
+    ctx.fillStyle = "black";
+    ctx.fillText(line, x + padding, y + padding - (index * lineHeight));
+  });
+
+  imageReader.addEventListener("load", (e) => {
+    currentImage = new Image();
+    currentImage.addEventListener("load", () => repaintImage());
+    currentImage.src = e.target.result;
   });
 };
 
 const buttonRandom = document.getElementById("randomize");
-buttonRandom.onclick = () => initImage();
+buttonRandom.addEventListener("click", async () => {
+  rerollText();
+  await rerollImage();
+  repaintImage();
+});
+
+const buttonRandomImg = document.getElementById("randomize-img");
+buttonRandomImg.addEventListener("click", async () => {
+  await rerollImage();
+  repaintImage();
+});
+
+const buttonRandomText = document.getElementById("randomize-text");
+buttonRandomText.addEventListener("click", () => {
+  rerollText();
+  repaintImage();
+});
+
+const inputCustomImg = document.getElementById("customImage");
+inputCustomImg.addEventListener("change", (e) => {
+  e.preventDefault();
+  if (e.target.files.length <= 0) {
+    return;
+  }
+  setFile(e.target.files[0]);
+});
+const buttonCustomImg = document.getElementById("customImageBtn");
+buttonCustomImg.addEventListener("click", () => {
+  inputCustomImg.click();
+});
 
 const inputCustom = document.getElementById("customText");
-const buttonCustom = document.getElementById("submitCustomText");
-buttonCustom.onclick = () => {
-  if (inputCustom.value) {
-    initImage(inputCustom.value);
+const replaceWithCustomText = async (e) => {
+  if (e.type === "input" || inputCustom.value) {
+    currentText = inputCustom.value;
+    repaintImage();
   }
 };
+inputCustom.addEventListener("click", replaceWithCustomText);
+inputCustom.addEventListener("input", replaceWithCustomText);
+
+const downloadLinkReal = document.createElement("a");
+downloadLinkReal.setAttribute("download", "VeselaKampan.jpg");
+const linkSave = document.getElementById("save");
+linkSave.addEventListener("click", (e) => {
+  e.preventDefault();
+  downloadLinkReal.setAttribute("href", canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream"));
+  downloadLinkReal.click();
+});
 
 initFont();
-initImage();
+
+rerollText();
+rerollImage()
+  .then(() => repaintImage());
